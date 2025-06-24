66050695d2746323fec303e40192b29df68afb7c
Max Lichter <iammaxlichter@gmail.com>
2025-06-23 23:44:28 -0500

    done with some features

--- js/index.js
+++ js/index.js
@@ -3,11 +3,15 @@ import { initBanner }   from './banner.js';
 import { loadFeatures } from './features.js';
 import { loadPromo }    from './promo.js';
 import { loadFooter }   from './footer.js';
+import { loadCustomize } from './customize.js';
+import { loadUniversal } from './universal.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   loadNavbar();
   initBanner();
   loadFeatures();
   loadPromo();
+  loadCustomize();
+  loadUniversal();
   loadFooter();
 });
